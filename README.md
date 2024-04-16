3760bb5647adf98e9abc4b784eae81971796cc53
ssy-lbh <59327856+ssy-lbh@users.noreply.github.com>
2024-04-16 11:06:11 +0800

    update README.md

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 SDK版本：API 29 Android 10.0 (Q)
 
 开发工具：Android Studio 或者 Jetbrains Idea
+
+```
+compileSdkVersion 34
+buildToolsVersion "30.0.0"
+
+targetSdkVersion 30
+```
+
+Gradle：5.6.4
+
+Java版本：1.8
--- app/src/main/java/com/crystalmath/WuhanMetro/MainActivity.java
+++ app/src/main/java/com/crystalmath/WuhanMetro/MainActivity.java
@@ -4,6 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/*
+当前任务列表：
+1. 对接地图SDK，建议百度地图
+2. 开发设置页面，能设置偏好，能保存到文件中
+3. 收集地铁数据，给出至少六条地铁路线的站点列表，包含经纬度、一天中人流量变化、少量事故信息等
+*/
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
