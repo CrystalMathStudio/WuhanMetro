522ef6fa5a6e35a9d500ce6ee3399e744d9af6f8
AnqiNie <131529689+AnqiNie@users.noreply.github.com>
2024-05-06 22:10:09 +0800

    replace

--- app/src/main/java/com/crystalmath/WuhanMetro/MainActivity.java
+++ app/src/main/java/com/crystalmath/WuhanMetro/MainActivity.java
@@ -2,11 +2,15 @@ package com.crystalmath.WuhanMetro;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.crystalmath.WuhanMetro.frag.BaiduMapFragment;
+import com.crystalmath.WuhanMetro.frag.MyFragment;
+import com.crystalmath.WuhanMetro.frag.PreferencesFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 /*
@@ -19,27 +23,58 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView navigationView;
-
+    private FragmentContainerView fragmentContainer;
+    Fragment fragment1 = new BaiduMapFragment();
+    Fragment fragment2 = new MyFragment();
+    Fragment fragment3=new PreferencesFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        fragmentContainer = findViewById(R.id.fragmentContainerView);
         navigationView = findViewById(R.id.bottom_navi);
         navigationView.setItemIconTintList(null);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragmentContainerView, fragment1)
+                .add(R.id.fragmentContainerView, fragment2)
+                .add(R.id.fragmentContainerView, fragment3)
+                        .commit();
+
         navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()){
                     case R.id.menu_home:
+                        replaceFragment(fragment1);
                         break;
                     case R.id.menu_set:
+                        replaceFragment(fragment2);
                         break;
                     case R.id.menu_my:
+                        replaceFragment(fragment3);
                         break;
                 }
                 return true;
             }
         });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        // 添加FragmentA到容器中
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainerView, fragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+            getSupportFragmentManager().popBackStack(); // 从回退栈中弹出最近的一个Fragment状态并显示它
+        } else {
+            super.onBackPressed(); // 如果回退栈为空，则调用父类的onBackPressed方法关闭Activity
+        }
     }
 }
